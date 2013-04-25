c66e14cba000de7695ccfd7c1f6de0795b9a6487
Lorenz Aebi <github@xca.ch>
2013-04-25 19:42:23 +0200

    instead of use the file make a function out of it, makes more sense when importing this file

--- terra/modem_off.py
+++ terra/modem_off.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 import time
 import ablib
- 
-print "Modem OFF"
- 
-quectel_power = ablib.Pin('W','10','low')
-quectel_power_key = ablib.Pin('E','10','low')
+
+def modem_off():
+	print "Modem OFF"
+
+	quectel_power = ablib.Pin('W','10','low')
+	quectel_power_key = ablib.Pin('E','10','low')
+
+
+if __name__ == '__main__':
+	modem_off()
 
--- terra/modem_on.py
+++ terra/modem_on.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 import time
 import ablib
- 
-print "Modem ON"
- 
-quectel_power = ablib.Pin('W','10','high')
-quectel_power_key = ablib.Pin('E','10','low')
 
-quectel_power_key.on()
-time.sleep(1)
-quectel_power_key.off()
+def modem_on():
+	print "Modem ON"
+
+	quectel_power = ablib.Pin('W','10','high')
+	quectel_power_key = ablib.Pin('E','10','low')
+
+	quectel_power_key.on()
+	time.sleep(1)
+	quectel_power_key.off()
+
+
+if __name__ == '__main__':
+	modem_on()
 
